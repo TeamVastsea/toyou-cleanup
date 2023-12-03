31fb6560dfaef17307f62dcf2519b7df0e9b241d
zrll_ <zhengruilin0@126.com>
2023-12-03 13:10:13 +0800

    add

---
+++ src/config.rs
@@ -0,0 +1,40 @@
+use std::fs::OpenOptions;
+use std::io::{Read, Write};
+use serde::{Serialize, Deserialize};
+use serde_inline_default::serde_inline_default;
+use tracing::{error, info};
+
+#[serde_inline_default]
+#[derive(Serialize, Deserialize, Debug)]
+pub struct ServerConfig {
+    #[serde_inline_default(String::from("mysql://toyou:tuyou123@localhost/tuyou"))]
+    pub url: String,
+    #[serde_inline_default(String::from("info"))]
+    pub trace_level: String,
+    #[serde_inline_default(false)]
+    pub sqlx_debug: bool,
+}
+
+pub fn get_config() -> ServerConfig {
+    let mut raw_config = String::new();
+    let mut file = OpenOptions::new().read(true).write(true).create(true).open("config.toml").expect("Cannot open 'config.toml'");
+    file.read_to_string(&mut raw_config).unwrap();
+
+    let config: ServerConfig = toml::from_str(&raw_config).unwrap();
+
+    if toml::to_string_pretty(&config).unwrap() != raw_config {
+        save(&config)
+    }
+
+    config
+}
+
+pub fn save(config: &ServerConfig) {
+    error!("Config changed, please edit and restart");
+    let config_str = toml::to_string_pretty(config).unwrap();
+
+    let mut file = OpenOptions::new().write(true).truncate(true).open("config.toml").expect("Cannot open 'config.toml'");
+    file.write(config_str.as_bytes()).unwrap();
+
+    panic!("config changed");
+}
